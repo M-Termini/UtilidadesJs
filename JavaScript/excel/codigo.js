793544378a7953de1fc66912208566bebae3547d
M-Termini <mariogterminic@gmail.com>
2025-12-14 20:05:02 -0400

    Nueva rama

--- JavaScript/excel/codigo.js
+++ JavaScript/excel/codigo.js
@@ -159,9 +159,5 @@ $body.addEventListener('click', e => {
     })
 
 });
-<<<<<<< HEAD
 console.log('Inicializando hoja de cálculo...');
-=======
-console.log("Todo bien")
->>>>>>> dbbd31ea4a02077cf1e217dd4e0d620f573513ff
 renderSheet()
